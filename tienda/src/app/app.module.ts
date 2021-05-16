424befad826342e85b6d6910d04e89dd001c051e
Dionisio Fernández <dionisio.fernandez.ortiz@gmail.com>
2021-05-16 18:06:20 +0200

    WIP: Inicio Nav-bar

--- tienda/src/app/app.module.ts
+++ tienda/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
